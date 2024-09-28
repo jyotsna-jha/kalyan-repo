69271d83437b920b36487d39ebe307080bb540f1
jyotsna-jha980 <jyotsna@builderlytics.ai>
2024-09-28 12:06:50 +0545

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+         
+        
+          <a href="https://www.linkedin.com/in/kalyan-kumar-shrestha-304309295/" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+         
+          <a href="https://github.com/Learneer" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="sthakumar444@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
